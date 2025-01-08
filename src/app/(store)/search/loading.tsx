14e7f71c45c10a8f93582b7338e24b66a053e607
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2025-01-08 17:22:41 -0300

    Corrigindo o erro do Build:  useSearchParams() should be wrapped in a suspense boundary at page

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,26 +1,37 @@
 'use client'
 
-import { Skeleton } from "@/components/skeleton"
+import { Suspense } from "react"
 import { useSearchParams } from "next/navigation"
+import { Skeleton } from "@/components/skeleton"
 
-export default function HomeLoading() {
+function SearchLoadingComponent() {
     const searchParams = useSearchParams()
 
     const query = searchParams.get('q')
 
     return (
-        <div className="flex flex-col gap-4">
-            <p className="text-sm">
-                Resultados para: <span className="font-semibold">{query}</span>
-            </p>
-            <div className="grid grid-cols-3 gap-6">
-                <Skeleton className="h-[435px]" />
-                <Skeleton className="h-[435px]" />
-                <Skeleton className="h-[435px]" />
-                <Skeleton className="h-[435px]" />
-                <Skeleton className="h-[435px]" />
-                <Skeleton className="h-[435px]" />
+        <Suspense>
+            <div className="flex flex-col gap-4">
+                <p className="text-sm">
+                    Resultados para: <span className="font-semibold">{query}</span>
+                </p>
+                <div className="grid grid-cols-3 gap-6">
+                    <Skeleton className="h-[435px]" />
+                    <Skeleton className="h-[435px]" />
+                    <Skeleton className="h-[435px]" />
+                    <Skeleton className="h-[435px]" />
+                    <Skeleton className="h-[435px]" />
+                    <Skeleton className="h-[435px]" />
+                </div>
             </div>
-        </div>
+        </Suspense>
+    )
+}
+
+export default function SearchLoading() {
+    return (
+        <Suspense>
+            <SearchLoadingComponent />
+        </Suspense>
     )
 }
--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import type { FormEvent } from "react";
+import { Suspense, type FormEvent } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Search } from "lucide-react";
 
-export function SearchForm() {
+function SearchFormComponent() {
     const router = useRouter() //apenas client; import "next/navigation"
     const searchParams = useSearchParams() //apenas client; import "next/navigation"
 
@@ -26,21 +26,31 @@ export function SearchForm() {
     }
 
     return (
-        <form
-            className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700"
-            onSubmit={handleSearch}
-        >
-            <Search
-                className="w-5 h-5 text-zinc-500"
-            />
+        <Suspense>
+            <form
+                className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700"
+                onSubmit={handleSearch}
+            >
+                <Search
+                    className="w-5 h-5 text-zinc-500"
+                />
 
-            <input
-                className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
-                name="q"
-                type="text"
-                placeholder="Buscar produtos..."
-                defaultValue={query ?? ''}
-            />
-        </form>
+                <input
+                    className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
+                    name="q"
+                    type="text"
+                    placeholder="Buscar produtos..."
+                    defaultValue={query ?? ''}
+                />
+            </form>
+        </Suspense>
+    )
+}
+
+export function SearchForm() {
+    return (
+        <Suspense>
+            <SearchFormComponent />
+        </Suspense>
     )
 }
