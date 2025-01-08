d202d4be6e9bd6b406ba335b535a0668ef52f175
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2025-01-08 18:30:40 -0300

    fix: Add suspense boundaries when using useSearchParams (#1)

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -10,27 +10,25 @@ function SearchLoadingComponent() {
     const query = searchParams.get('q')
 
     return (
-        <Suspense>
-            <div className="flex flex-col gap-4">
-                <p className="text-sm">
-                    Resultados para: <span className="font-semibold">{query}</span>
-                </p>
-                <div className="grid grid-cols-3 gap-6">
-                    <Skeleton className="h-[435px]" />
-                    <Skeleton className="h-[435px]" />
-                    <Skeleton className="h-[435px]" />
-                    <Skeleton className="h-[435px]" />
-                    <Skeleton className="h-[435px]" />
-                    <Skeleton className="h-[435px]" />
-                </div>
+        <div className="flex flex-col gap-4">
+            <p className="text-sm">
+                Resultados para: <span className="font-semibold">{query}</span>
+            </p>
+            <div className="grid grid-cols-3 gap-6">
+                <Skeleton className="h-[435px]" />
+                <Skeleton className="h-[435px]" />
+                <Skeleton className="h-[435px]" />
+                <Skeleton className="h-[435px]" />
+                <Skeleton className="h-[435px]" />
+                <Skeleton className="h-[435px]" />
             </div>
-        </Suspense>
+        </div>
     )
 }
 
 export default function SearchLoading() {
     return (
-        <Suspense>
+        <Suspense fallback={null}>
             <SearchLoadingComponent />
         </Suspense>
     )
